2ddde7d4eb2ac1fc3ad7962f4b27e33157f48ebd
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-10-28 21:16:18 +0300

    feat: commands

---
+++ src/main/kotlin/ru/quipy/logic/commands/user/UserCommands.kt
@@ -0,0 +1,21 @@
+package ru.quipy.logic.commands.user
+
+import at.favre.lib.crypto.bcrypt.BCrypt
+import ru.quipy.api.aggregates.events.user.UserCreatedEvent
+import ru.quipy.api.aggregates.events.user.UserNameChangedEvent
+import ru.quipy.logic.states.UserAggregateState
+import java.lang.IllegalArgumentException
+import java.util.*
+
+fun UserAggregateState.create(nickname: String, userName: String?, password: String): UserCreatedEvent {
+    val hashPassword = BCrypt.withDefaults().hashToString(10, password.toCharArray())
+    return UserCreatedEvent(UUID.randomUUID(), nickname, userName, hashPassword)
+}
+
+fun UserAggregateState.changeName(currentUserId: UUID, userName: String?): UserNameChangedEvent {
+    if (currentUserId != getId()) {
+        throw IllegalArgumentException("User $currentUserId can not change name of ${getId()}")
+    }
+
+    return UserNameChangedEvent(currentUserId, userName)
+}
