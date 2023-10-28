88585411ddf0fe9e5f652e2bec79ec38371770ad
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2023-10-28 21:41:40 +0300

    feat: subscribers

--- src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
+++ src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
@@ -9,9 +9,7 @@ import ru.quipy.api.aggregates.UserAggregate
 import ru.quipy.core.EventSourcingServiceFactory
 import ru.quipy.logic.states.ProjectAggregateState
 import ru.quipy.logic.states.UserAggregateState
-import ru.quipy.projections.AnnotationBasedProjectEventsSubscriber
 import ru.quipy.streams.AggregateEventStreamManager
-import ru.quipy.streams.AggregateSubscriptionsManager
 import java.util.*
 import javax.annotation.PostConstruct
 
@@ -41,20 +39,11 @@ class EventSourcingLibConfiguration {
     private val logger = LoggerFactory.getLogger(EventSourcingLibConfiguration::class.java)
 
     @Autowired
-    private lateinit var subscriptionsManager: AggregateSubscriptionsManager
-
-    @Autowired
-    private lateinit var projectEventSubscriber: AnnotationBasedProjectEventsSubscriber
-
-    @Autowired
     private lateinit var eventSourcingServiceFactory: EventSourcingServiceFactory
 
     @Autowired
     private lateinit var eventStreamManager: AggregateEventStreamManager
 
-    /**
-     * Use this object to create/update the aggregate
-     */
     @Bean
     fun userEsService() = eventSourcingServiceFactory.create<UUID, UserAggregate, UserAggregateState>()
 
@@ -63,10 +52,6 @@ class EventSourcingLibConfiguration {
 
     @PostConstruct
     fun init() {
-        // Demonstrates how to explicitly subscribe the instance of annotation based subscriber to some stream. See the [AggregateSubscriptionsManager]
-        subscriptionsManager.subscribe<ProjectAggregate>(projectEventSubscriber)
-
-        // Demonstrates how you can set up the listeners to the event stream
         eventStreamManager.maintenance {
             onRecordHandledSuccessfully { streamName, eventName ->
                 logger.info("Stream $streamName successfully processed record of $eventName")
